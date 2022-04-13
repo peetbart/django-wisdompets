e4ded75f4d59ca7c38026aaf886e92fbe8114602
Peter Bartfay <peter.bartfay@hogarthww.com>
2022-04-13 17:29:06 +0100

    name fix

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Pet(models.Model):
 	SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
-	name = models.Charfield(max_length=100)
-	submitter = models.Charfield(max_length=100)
-	species = models.Charfield(max_length=30)
-	breed = models.Charfield(max_length=30, blank=True)
+	name = models.CharField(max_length=100)
+	submitter = models.CharField(max_length=100)
+	species = models.CharField(max_length=30)
+	breed = models.CharField(max_length=30, blank=True)
 	description = models.TextField()
 	sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
 	submission_date = models.DateTimeField()
@@ -15,4 +15,4 @@ class Pet(models.Model):
 
 
 class Vaccine(models.Model):
-	name = models.Charfield(max_length=50)
+	name = models.CharField(max_length=50)
